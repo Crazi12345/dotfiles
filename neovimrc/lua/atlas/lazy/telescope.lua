edd3dc41096d8733c9eeb7e268f5aed3b09cfbea
Patrick <patrickbjerre@outlook.dk>
2024-07-25 22:56:38 +0200

    better telescope

--- neovimrc/lua/atlas/lazy/telescope.lua
+++ neovimrc/lua/atlas/lazy/telescope.lua
@@ -1,7 +1,7 @@
 return {
     "nvim-telescope/telescope.nvim",
 
-    tag = "0.1.5",
+    tag = "0.1.8",
 
     dependencies = {
         "nvim-lua/plenary.nvim"
@@ -12,6 +12,13 @@ return {
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>pF', builtin.treesitter, {})
+        vim.keymap.set('n', '<leader>ch', builtin.colorscheme, {})
+        vim.keymap.set('n', '<leader>PP', builtin.planets, {})
+        vim.keymap.set('n', '<leader>DD', builtin.diagnostics, {})
+        vim.keymap.set('n', '<leader>QF', builtin.quickfix, {})
+        vim.keymap.set('n', '<leader>IG', builtin.lsp_outgoing_calls, {})
+
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>pws', function()
             local word = vim.fn.expand("<cword>")
@@ -22,9 +29,12 @@ return {
             builtin.grep_string({ search = word })
         end)
         vim.keymap.set('n', '<leader>ps', function()
+            builtin.live_grep({  })
+        end)
+        vim.keymap.set('n', '<leader>pS', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
+
         vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
     end
 }
-
